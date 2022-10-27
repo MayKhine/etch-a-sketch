d9e6da8e706760572963fffa2cbb78d33ea31ada
May <maykhine.ada@gmail.com>
2022-10-27 14:38:33 -0400

    add eraser, pen color picker, background color picker, rainbow pen, grid size slider

--- etchASketch.js
+++ etchASketch.js
@@ -1,6 +1,10 @@
 const containerEle = document.querySelector(".container");
-let gridSize = 9;
+let gridSize = 16;
 let trigger = false;
+let penColor = '#000000';
+let canvasColor = '#FFC0CB';
+let selectionMode = 'pen';
+let gridState = 1;
 
 const createDiv = (className) => {
     let div = document.createElement("div");
@@ -16,6 +20,7 @@ const createGrid = (num) => {
     const gridItems = []
     for (let i = 0; i < num * num; i++) {
         let gridItem = createDiv("grid-item");
+        gridItem.style.backgroundColor = canvasColor; //set canvas color 
         gridContainerDiv.appendChild(gridItem);
         gridItems.push(gridItem)
     }
@@ -31,19 +36,31 @@ const updateGrid = (gridSize) => {
         item.remove()
     })
     createGrid(gridSize)
+    setCanvas('grid', gridState)
+}
+
+const getPenColor = () => {
+    if (selectionMode == 'eraser') {
+        return canvasColor
+    }
+    else if (selectionMode == 'pen') {
+        return penColor
+    } else if (selectionMode == 'rainbow') {
+        return selectRandomRGB()
+    }
 }
 
 const enableSketch = (gridItems) => {
     gridItems.forEach((item) => {
         item.addEventListener('mousedown', () => {
             // console.log(sketch)
-            item.style.backgroundColor = 'black'
+            item.style.backgroundColor = getPenColor()
             trigger = true;
             // sketch(gridItems)
         })
         item.addEventListener('mouseover', () => {
             if (trigger) {
-                item.style.backgroundColor = 'black'
+                item.style.backgroundColor = getPenColor()
             }
 
         })
@@ -53,42 +70,208 @@ const enableSketch = (gridItems) => {
     })
 }
 
-//create grid board
+const setSelectionMode = (mode) => {
+    selectionMode = mode;
+
+    if (mode == 'eraser') {
+        eraserButton.style.background = 'darkgray'
+        penButton.style.background = 'lightgray'
+        rainbowPenButton.style.background = 'lightgray'
+    } else if (mode == 'pen') {
+        penButton.style.background = 'darkgray'
+        eraserButton.style.background = 'lightgray'
+        rainbowPenButton.style.background = 'lightgray'
+    } else if (mode == 'rainbow') {
+        rainbowPenButton.style.background = 'darkgray'
+        eraserButton.style.background = 'lightgray'
+        penButton.style.background = 'lightgray'
+    }
+}
+
+const setCanvas = (mode, value) => {
+    let curGridItems = document.querySelectorAll('.grid-item')
+    curGridItems.forEach((item) => {
+
+        if (mode == 'grid') {
+            if (value == 1) {
+                item.style.border = "1px    solid   white"
+            }
+            else {
+                item.style.border = "0px    solid   white"
+            }
+        } else if (mode == 'canvas') {
+            item.style.backgroundColor = canvasColor
+        }
+    })
+}
+
+const selectRandomRGB = () => {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    return (`rgb(${x},${y},${z})`)
+}
+
+//create grid board div
 let gridContainerDiv = createDiv("grid-container");
 containerEle.appendChild(gridContainerDiv);
-createGrid(gridSize);
 
 //add user selection section 
-const userSelection = createDiv("userSelection")
+const userSelection = createDiv("user-selection")
 containerEle.appendChild(userSelection)
 
+//create canvs section 
+const canvasSection = createDiv("canvas")
+userSelection.appendChild(canvasSection)
+
+//create grid size selection 
+const gridSizeSelectSection = createDiv("grid-size-section")
+canvasSection.appendChild(gridSizeSelectSection)
 //create grid size range slider
-const gridSizeSlier = document.createElement("input")
-gridSizeSlier.setAttribute('type', 'range')
-gridSizeSlier.setAttribute('min', '1')
-gridSizeSlier.setAttribute('max', '100')
-gridSizeSlier.setAttribute('value', gridSize)
-userSelection.appendChild(gridSizeSlier)
+const gridSizeSlider = document.createElement("input")
+gridSizeSlider.setAttribute('type', 'range')
+gridSizeSlider.setAttribute('min', '1')
+gridSizeSlider.setAttribute('max', '100')
+gridSizeSlider.setAttribute('value', gridSize)
+gridSizeSelectSection.appendChild(gridSizeSlider)
 const gridSizeSelection = document.createElement("div")
-gridSizeSelection.innerHTML = `${gridSize} x ${gridSize}`
-userSelection.appendChild(gridSizeSelection)
+gridSizeSelection.innerHTML = `Grid size: ${gridSize} x ${gridSize}`
+gridSizeSelectSection.appendChild(gridSizeSelection)
+
+//create canvas color section 
+const canvasColorSection = createDiv('canvas-color-section')
+canvasColorSection.innerHTML = 'Canvas Color'
+canvasSection.appendChild(canvasColorSection)
+//create canvas color picker
+const canvasColorPicker = document.createElement("input")
+canvasColorPicker.classList.add('color-picker')
+canvasColorPicker.setAttribute('type', 'color')
+canvasColorPicker.setAttribute('value', canvasColor)
+canvasColorSection.appendChild(canvasColorPicker)
+
+//grid line 
+// const gridLineSection = createDiv('grid-line-section')
+// userSelection.appendChild(gridLineSection)
+//create eraser button 
+const gridButton = document.createElement('button')
+gridButton.classList.add('button')
+gridButton.innerText = 'Grid Line'
+gridButton.style.backgroundColor = 'darkgray'
+canvasSection.appendChild(gridButton)
+
+
+//create pen color section 
+const penColorSection = createDiv('pen-color-section')
+penColorSection.innerHTML = 'Pen Color'
+userSelection.appendChild(penColorSection)
+//create pen color picker
+const penColorPicker = document.createElement("input")
+
+penColorPicker.classList.add('color-picker')
+penColorPicker.setAttribute('type', 'color')
+penColorPicker.setAttribute('value', penColor)
+penColorSection.appendChild(penColorPicker)
+
+// pen
+// const penSection = createDiv('pen-section')
+// penColorSection.appendChild(penSection)
+//create pen button 
+const penButton = document.createElement('button')
+penButton.classList.add('button')
+penButton.innerText = 'Drawing Pen'
+penColorSection.appendChild(penButton)
+//rainbow pen
+const rainbowPenButton = document.createElement('button')
+rainbowPenButton.classList.add('button')
+rainbowPenButton.innerText = 'Rainbow Pen'
+penColorSection.appendChild(rainbowPenButton)
+
+// eraser
+const eraserSection = createDiv('eraser-section')
+userSelection.appendChild(eraserSection)
+//create eraser button 
+const eraserButton = document.createElement('button')
+eraserButton.classList.add('button')
+eraserButton.innerText = 'Eraser'
+eraserSection.appendChild(eraserButton)
+
+
+
+// ------------------------- actions 
+createGrid(gridSize);
+setSelectionMode(selectionMode);
+setCanvas('grid', gridState)
+
+let slider;
+gridSizeSlider.addEventListener('mousedown', () => {
+    slider = 'clicked';
+    // console.log('Mouse down')
+})
+
+gridSizeSlider.addEventListener('mousemove', (event) => {
+    // console.log('mouse move ', slider)
+    gridSize = event.target.value
+    if (slider == 'clicked') {
+        gridSizeSelection.innerHTML = `${gridSize} x ${gridSize}`
+    }
+})
 
 //update grid size by user selection 
-gridSizeSlier.addEventListener('mouseup', (slider) => {
+gridSizeSlider.addEventListener('mouseup', (event) => {
     // console.log(slider.target.value)
-    gridSize = slider.target.value
+    gridSize = event.target.value
     //update the grid size text
-    gridSizeSelection.innerHTML = `${gridSize} x ${gridSize}`
-
-    // gridSizeSlier.setAttribute('placeholder', slider.target.value)
+    // gridSizeSelection.innerHTML = `${gridSize} x ${gridSize}`
     updateGrid(gridSize)
-    //select the latest grid items and draw
-    // gridItems = document.querySelectorAll('.grid-item')
-    // sketch(gridItems)
 })
 
 
-// eraser 
+canvasColorPicker.addEventListener('input', (event) => {
+    canvasColor = event.target.value
+    setCanvas('canvas', canvasColor)
+})
+
+
+penColorPicker.addEventListener('input', (event) => {
+    penColor = event.target.value
+})
+
+penButton.addEventListener('click', (event) => {
+    // selectionMode = 'pen'
+    // penButton.style.background = 'darkgray'
+    // eraserButton.style.background = 'lightgray'
+    setSelectionMode('pen')
+})
+
+rainbowPenButton.addEventListener('click', (event) => {
+    setSelectionMode('rainbow')
+})
+
+eraserButton.addEventListener('click', (event) => {
+    // selectionMode = 'eraser'
+    // eraserButton.style.background = 'darkgray'
+    // penButton.style.background = 'lightgray'
+    setSelectionMode('eraser')
+})
+
+gridButton.addEventListener('click', (event) => {
+    //grid state 
+    // if grid is on, off it 
+    if (gridState == 1) {
+        gridState = 0
+        setCanvas('grid', 0)
+        gridButton.style.backgroundColor = 'lightgray'
+    } else {
+        gridState = 1
+        setCanvas('grid', 1)
+        gridButton.style.backgroundColor = 'darkgray'
+
+    }
+})
+
+
+
+
 // pick canvas color
 // pick grid item color
 
